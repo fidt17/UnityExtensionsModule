5112ffdb4a0a5ddc965df3b6a962dfce8530e75a
Vladimir <fidt17@gmail.com>
2022-07-25 13:01:06 +0300

    Remove tests folder from package

---
+++ Assets/fidt17/UnityExtensionsModule/Runtime/CoroutineExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace fidt17.UnityExtensionsModule.Runtime
+{
+    public static class CoroutineExtensions
+    {
+        /// <summary>
+        /// Schedule an action to execute on complete.
+        /// </summary>
+        public static IEnumerator SetOnComplete(this IEnumerator enumerator, Action action)
+        {
+            yield return enumerator;
+            action?.Invoke();
+        }
+
+        public static IEnumerator SetOnComplete(this Coroutine coroutine, Action action)
+        {
+            yield return coroutine;
+            action?.Invoke();
+        }
+    }
+}
--- Assets/fidt17/UnityExtensionsModule/Runtime/TweenExtensions.cs
+++ Assets/fidt17/UnityExtensionsModule/Runtime/TweenExtensions.cs
@@ -1,15 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace fidt17.UnityExtensionsModule.Runtime
 {
     public static class TweenExtensions
     {
+        /// <summary>
+        /// Enumerator that last for *DURATION* seconds.
+        /// After each yield calls *PROGRESS_ACTION* with progress input (0 -> 1).
+        /// Can specify yield function.
+        /// </summary>
+        public static IEnumerator Progress(float duration, Action<float> progressAction, Func<IEnumerator> yieldFunc = null)
+        {
+            for (float t = 0; t < duration; t += Time.deltaTime)
+            {
+                var progress = t / duration;
+                progressAction?.Invoke(progress);
+                yield return yieldFunc;
+            }
+            progressAction?.Invoke(1);
+        }
+
+        /// <summary>
+        /// Enumerator that last for *DURATION* seconds.
+        /// After each yield calls *PROGRESS_ACTION* with progress input (1 -> 0).
+        /// Can specify yield function.
+        /// </summary>
+        public static IEnumerator ReverseProgress(float duration, Action<float> progressAction, Func<IEnumerator> yieldFunc = null)
+        {
+            for (float t = 0; t < duration; t += Time.deltaTime)
+            {
+                var progress = t / duration;
+                progressAction?.Invoke(1-progress);
+                yield return yieldFunc;
+            }
+            progressAction?.Invoke(0);
+        }
+        
         #region Move
         
         public static IEnumerator Move(Transform transform, Vector3 start, Vector3 target, float duration)
         {
-            yield return EnumeratorExtensions.Progress(duration, progress =>
+            yield return Progress(duration, progress =>
             {
                 transform.position = Vector3.Lerp(start, target, progress);
             });
@@ -18,7 +51,7 @@ namespace fidt17.UnityExtensionsModule.Runtime
         public static IEnumerator Move(Transform transform, Vector3 target, float duration)
         {
             var start = transform.position;
-            yield return EnumeratorExtensions.Progress(duration, progress =>
+            yield return Progress(duration, progress =>
             {
                 transform.position = Vector3.Lerp(start, target, progress);
             });
@@ -30,7 +63,7 @@ namespace fidt17.UnityExtensionsModule.Runtime
         
         public static IEnumerator Scale(Transform transform, Vector3 start, Vector3 target, float duration)
         {
-            yield return EnumeratorExtensions.Progress(duration, progress =>
+            yield return Progress(duration, progress =>
             {
                 transform.localScale = Vector3.Lerp(start, target, progress);
             });
@@ -39,7 +72,7 @@ namespace fidt17.UnityExtensionsModule.Runtime
         public static IEnumerator Scale(Transform transform, Vector3 target, float duration)
         {
             var start = transform.localScale;
-            yield return EnumeratorExtensions.Progress(duration, progress =>
+            yield return Progress(duration, progress =>
             {
                 transform.localScale = Vector3.Lerp(start, target, progress);
             });
@@ -48,7 +81,7 @@ namespace fidt17.UnityExtensionsModule.Runtime
         public static IEnumerator ScaleY(Transform transform, float target, float duration)
         {
             var start = transform.localScale;
-            yield return EnumeratorExtensions.Progress(duration, progress =>
+            yield return Progress(duration, progress =>
             {
                 transform.localScale = new Vector3(start.x, Mathf.Lerp(start.y, target, progress), start.z);
             });
